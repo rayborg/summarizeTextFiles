9bf5044128bc63628820760c0d314fb0d8b50fef
Raymond C. Borges Hink <borgesraymond@gmail.com>
2020-01-06 04:55:47 -0500

    Added comments

--- pdf2Text.sh
+++ pdf2Text.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#First install pdfminer with: pip install pdfminer
+
+#Create a directory called text then run this script in same folder as your PDFs
 for f in *.pdf; do
   pdf2txt.py "$f" >> ./text/"${f%}.txt"
 done
